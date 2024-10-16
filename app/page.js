3f5fb165ca873d83808d235a98f43fea5ecaa2e7
sokada <sokada1@csuchico.edu>
2024-10-16 12:42:04 -0700

    changed page

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-sm font-sans mb-4 text-center">Beginner App</h1>
-      <h1 className="text6xl font-sans font-normal text-center">Start everything from here</h1>
-      <h1 className="text-xl font-sans font-normal m-0 text-center">Free to use. Easy to try.</h1>
+      <h1 className="text-xl font-sans font-normal m-0 text-center">Start everything from here</h1>
+      <h1 className="text-6xl font-sans font-normal m-0 text-center">Free to use. Easy to try.</h1>
       <SearchContainer />
     </main>
   );
