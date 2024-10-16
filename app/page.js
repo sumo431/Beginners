af62337e858c8605de776fc59f1aa6ef6ea0549e
sokada <sokada1@csuchico.edu>
2024-10-16 12:15:24 -0700

    change font

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import SearchContainer from './components/Searchbar';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold mb-8 text-center">Beginner App</h1>
+      <h1 className="text-4xl font-mono mb-8 text-center">Beginner App</h1>
       <SearchContainer />
     </main>
   );
