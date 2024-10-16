652832e69297ef9f02f5d8bc7d06eb0f9e8982b5
sokada <sokada1@csuchico.edu>
2024-10-16 12:34:43 -0700

    change page

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ import SearchContainer from './components/Searchbar';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-mono mb-8 text-center">Beginner App</h1>
-      <h1 className="text-4xl font-sans font-normal m-2 text-center">Free to use. Easy to try.</h1>
+      <h1 className="text-sm font-sans mb-4 text-center">Beginner App</h1>
+      <h1 className="text-2xl font-sans font-normal m-0 text-center">Free to use. Easy to try.</h1>
       <SearchContainer />
     </main>
   );
