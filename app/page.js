efafc45693361be32a5962d6f5cb74cd8810158e
sokada <sokada1@csuchico.edu>
2024-10-16 12:31:05 -0700

    add text

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl font-mono mb-8 text-center">Beginner App</h1>
+      <h1 className="text-4xl font-sans font-normal m-2 text-center">Free to use. Easy to try.</h1>
       <SearchContainer />
     </main>
   );
