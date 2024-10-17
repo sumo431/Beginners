e32dcb4a156b042fb318efdfd846767b7ce25ffd
sokada <sokada1@csuchico.edu>
2024-10-17 15:34:30 -0700

    changed hp

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 'use client';
 
 import SearchContainer from './components/Searchbar';
-import './style.css';
 
 export default function Home(){
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center p-24"
+    style={{ backgroundColor: 'plum' }}>
       <h1 className="text-sm font-sans mb-4 text-center">Beginner App</h1>
       <h1 className="text-3xl font-sans font-normal m-0 text-center">Start everything from here</h1>
       <SearchContainer />
