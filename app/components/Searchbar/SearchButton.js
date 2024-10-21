8d26723030c8e565d5a6e584e7561fe73aaba2ac
sokada <sokada1@csuchico.edu>
2024-10-21 12:57:55 -0700

    text changed

--- app/components/Searchbar/SearchButton.js
+++ app/components/Searchbar/SearchButton.js
@@ -1,10 +1,10 @@
 export default function SearchButton() {
   return (
     <button
-      className="flex-shrink-0 bg-white-500 hover:bg-white-700 border-white-500 hover:border-white-700 text-sm border-4 text-black py-1 px-2 rounded"
+      className="flex-shrink-0 bg-white hover:bg-grey-700 border-white hover:border-grey-700 text-sm border-4 text-black py-1 px-2 rounded"
       type="submit"
     >
-      Search
+      Search 
     </button>
   );
 }
