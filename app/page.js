50d76593731b2829cd39b6039d3ca270504d79cd
sokada <sokada1@csuchico.edu>
2024-10-21 12:28:04 -0700

    word changed

--- app/page.js
+++ app/page.js
@@ -5,11 +5,11 @@ import SearchContainer from './components/Searchbar';
 export default function Home(){
   return (
     <main className="flex min-h-screen flex-col items-center p-24"
-    style={{ backgroundColor: 'lightblue' }}>
-      <h1 className="text-sm font-sans mb-4 text-center">Beginner App</h1>
-      <h1 className="text-3xl font-sans font-normal m-0 text-center">Start everything from here</h1>
+    style={{ backgroundColor: 'black' }}>
+      <h1 className="text-sm font-sans mb-4 text-white text-center">Beginner App</h1>
+      <h1 className="text-3xl font-sans font-normal m-0 text-white text-center">Start everything from here</h1>
       <SearchContainer />
-      <h1 className="text-xl font-sans font-normal m-0 text-center">Free to use. Easy to try.</h1>
+      <h1 className="text-xl font-sans font-normal m-0 text-white text-center">Free to use. Easy to try.</h1>
     </main>
   );
 }
