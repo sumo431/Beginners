2dd317bb1e3ab4e3ad7fb6c98f0ad66f012a4c62
sokada <sokada1@csuchico.edu>
2024-10-23 12:00:46 -0700

    changed search button color

--- app/components/Searchbar/SearchButton.js
+++ app/components/Searchbar/SearchButton.js
@@ -1,7 +1,7 @@
 export default function SearchButton() {
   return (
     <button
-      className="flex-shrink-0 bg-white hover:bg-grey-700 border-white hover:border-grey-700 text-sm border-4 text-black py-1 px-2 rounded"
+      className="flex-shrink-0 bg-white hover:bg-gray-700 border-white hover:border-gray-700 text-sm border-4 text-black py-1 px-2 rounded"
       type="submit"
     >
       Search 
