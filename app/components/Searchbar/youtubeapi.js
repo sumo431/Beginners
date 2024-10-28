ef7c501ee7d6dbb23158798dfb31ac2d36fe5cf6
sokada <sokada1@csuchico.edu>
2024-10-28 13:00:26 -0700

    chnage font

--- app/components/Searchbar/youtubeapi.js
+++ app/components/Searchbar/youtubeapi.js
@@ -1,24 +1,23 @@
-'use client'
 /**
    * Sample JavaScript code for youtube.channels.list
    * See instructions for running APIs Explorer code samples locally:
    * https://developers.google.com/explorer-help/code-samples#javascript
    */
 
-function authenticate() {
+export default function authenticate() {
     return gapi.auth2.getAuthInstance()
         .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
         .then(function() { console.log("Sign-in successful"); },
               function(err) { console.error("Error signing in", err); });
   }
-  function loadClient() {
+export default function loadClient() {
     gapi.client.setApiKey("AIzaSyAo-BXFneyzCDj4TlDwbwJ0cE8c479iiPw");//this is our API key
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
         .then(function() { console.log("GAPI client loaded for API"); },
               function(err) { console.error("Error loading GAPI client for API", err); });
   }
   // Make sure the client is loaded and sign-in is complete before calling this method.
-  function execute() {
+export default function execute() {
     return gapi.client.youtube.channels.list({
       "id": [
         "UC_x5XG1OV2P6uZZ5FSM9Ttw"
