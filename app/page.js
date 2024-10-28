e3c1adaa5dbaf88c293af43ab989464f9d00b7e3
sokada <sokada1@csuchico.edu>
2024-10-28 13:17:42 -0700

    import font.css

--- app/page.js
+++ app/page.js
@@ -2,11 +2,12 @@
 
 
 import SearchContainer from './components/Searchbar';
+import './styles/globalfont.css';
 
 export default function Home(){
   return (
     <main className="flex min-h-screen flex-col items-center p-24 bg-black">
-      <h1 className="text-sm font-helvetica-w02-light mb-4 text-white text-center">Beginner App</h1>
+      <h1 className="text-sm font-danfo mb-4 text-white text-center">Beginner App</h1>
       <h2 className="text-3xl font-sans font-normal m-0 text-white text-center">Start everything from here</h2>
       <SearchContainer />
       <h2 className="text-xl font-sans font-normal m-0 text-white text-center">Free to use. Easy to try.</h2>
