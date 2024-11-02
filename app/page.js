8ab17c8a93cfd60b7e7141a973dccbf10efdd1ea
yoyeng83 <yyeng066@gmail.com>
2024-11-01 19:13:54 -0700

    Save changes to page.js

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
             {/* Boxes Section */}
             <div className="grid grid-cols-2 gap-4 mt-8">
                 {/* Box 1 */}
-                <div className="border p-4">
+                <div className="border p-4 h-64 relative">
                     <Link href="/cooking">
                         <h2 className="text-2xl text-white mb-2">Easy Cooking Recipes for Beginners</h2>
                         <Image src="/pictures/cooking.jpg" alt="Cooking" layout="responsive" width={500} height={300} className="object-cover" />
@@ -26,7 +26,7 @@ export default function Home() {
                 </div>
 
                 {/* Box 2 */}
-                <div className="border p-4">
+                <div className="border p-4 h-64 relative">
                     <Link href="/gamedevelop">
                         <h2 className="text-2xl text-white mb-2">Introduction to Game Development</h2>
                         <Image src="/pictures/gamedevelop.jpg" alt="Game Developing" layout="responsive" width={500} height={300} className="object-cover" />
@@ -34,7 +34,7 @@ export default function Home() {
                 </div>
 
                 {/* Box 3 */}
-                <div className="border p-4">
+                <div className="border p-4 h-64 relative">
                     <Link href="/guitar">
                         <h2 className="text-2xl text-white mb-2">Beginner's Guide to Playing Guitar</h2>
                         <Image src="/pictures/guitar.jpg" alt="Guitar" layout="responsive" width={500} height={300} className="object-cover" />
@@ -42,7 +42,7 @@ export default function Home() {
                 </div>
 
                 {/* Box 4 */}
-                <div className="border p-4">
+                <div className="border p-4 h-64 relative">
                     <Link href="/grilling">
                         <h2 className="text-2xl text-white mb-2">Grilling Tips for Newbies</h2>
                         <Image src="/pictures/grilling.jpg" alt="Grilling" layout="responsive" width={500} height={300} className="object-cover" />
@@ -50,7 +50,7 @@ export default function Home() {
                 </div>
 
                 {/* Box 5 */}
-                <div className="border p-4">
+                <div className="border p-4 h-64 relative">
                     <Link href="/math">
                         <h2 className="text-2xl text-white mb-2">Math Made Simple for Beginners</h2>
                         <Image src="/pictures/math.jpg" alt="Math" layout="responsive" width={500} height={300} className="object-cover" />
@@ -58,7 +58,7 @@ export default function Home() {
                 </div>
 
                 {/* Box 6 */}
-                <div className="border p-4">
+                <div className="border p-4 h-64 relative">
                     <Link href="/Phone">
                         <h2 className="text-2xl text-white mb-2">Getting Started with Your iPhone</h2>
                         <Image src="/pictures/iphone.jpg" alt="Using an iPhone" layout="responsive" width={500} height={300} className="object-cover" />
