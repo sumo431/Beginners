bb96ec558c2ee76d6e886338fe38749d1be16c49
yoyeng83 <yyeng066@gmail.com>
2024-11-01 19:18:25 -0700

    Add Phone.js

---
+++ app/Phone.js
@@ -0,0 +1,13 @@
+'use client';
+
+export default function Phone() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-3xl font-bold mb-4">How to Use Your Phone</h1>
+      <p className="text-lg">
+        Here you can add content about how to use your phone...
+      </p>
+      {/* You can add more content, images, or links here */}
+    </div>
+  );
+}
